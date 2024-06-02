14de0de651d4e7a4b94f2305349e549e9c559b8a
Nitish Kumar <nitish14920@gmail.com>
2024-06-03 03:12:57 +0530

    Bugs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import "./App.css";
 import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
+import React from "react";
 
 function App() {
   return (
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Footer = (props: string) => {
-  return <div>{props}</div>;
+const Footer = () => {
+  return <div>Footer</div>;
 };
 
 export default Footer;
