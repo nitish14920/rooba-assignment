779f6b655ce43fe0ef1aa361c06bf6377e8624b1
Nitish Kumar <nitish14920@gmail.com>
2024-07-10 18:22:54 +0530

    Products Temp api

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ImageCarousel from "./Carousel";
 import Categories from "./Categories/Categories";
+import "./body.css";
+import Products from "./ProductsSection/Products";
 
 const Body = () => {
   return (
     <div className="px-8 bg-gray-300 py-4">
       <Categories />
       <ImageCarousel />
+      <Products />
     </div>
   );
 };
